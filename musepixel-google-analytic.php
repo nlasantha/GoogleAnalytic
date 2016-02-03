8e1e751639f7d5a92c32e94722501940f74f4bc2
Nuwan <nlasantha@users.noreply.github.com>
2016-02-03 14:22:52 +0900

    Update and rename ganalytic.php to musepixel-google-analytic.php

--- musepixel-google-analytic.php
+++ musepixel-google-analytic.php
@@ -5,3 +5,8 @@ Plugin URI: http://musepixel.com
 Description: Adds a Google analytics trascking code to the <head> of your theme, by hooking to wp_head.
 Author: Nuwan Lasantha
 Version: 1.2
+
+function wpmudev_google_analytics() { ?>
+
+<?php }
+add_action( 'wp_head', 'wpmudev_google_analytics', 10 )
