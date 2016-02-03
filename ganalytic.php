b00e42e8997f514056ab54b92abe0dfe5221627d
Nuwan <nlasantha@users.noreply.github.com>
2016-02-03 14:21:21 +0900

    Create ganalytic.php

---
+++ ganalytic.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Simple Google Analytics Plugin
+Plugin URI: http://musepixel.com
+Description: Adds a Google analytics trascking code to the <head> of your theme, by hooking to wp_head.
+Author: Nuwan Lasantha
+Version: 1.2
